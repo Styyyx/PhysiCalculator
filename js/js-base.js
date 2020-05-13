a3fa9e5b73a94879386446eea178ce5b90f6ab78
Styyyx <46678157+Styyyx@users.noreply.github.com>
2020-05-13 23:18:16 +0800

    renamed js-home.js >> js-base.js

---
+++ js/js-base.js
@@ -0,0 +1,13 @@
+$('.panel#home a').on('click', () => {
+  $('.navbar').css('left', '0');
+});
+
+$('.navbar a[href="#home"]').on('click', ()=>{
+  $('.navbar').css('left', '-22.5vw');
+});
+
+// Prevents highlighting/selecting elements on drag
+$('*').attr('unselectable', 'on')
+	.css('user-select', 'none')
+  .bind('selectstart', function () { return false; });
+  
