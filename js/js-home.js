7b8aee5064577a8cbe6771d070bca3b3ddc61405
Styyyx <46678157+Styyyx@users.noreply.github.com>
2020-05-13 14:01:51 +0800

    All elements now unselectable

--- js/js-home.js
+++ js/js-home.js
@@ -3,5 +3,10 @@ $('.panel#home .button').on('click', () => {
 });
 
 $('.navbar a[href="#home"]').on('click', ()=>{
-  $('.navbar').css('left', '-25vw');
+  $('.navbar').css('left', '-22.5vw');
 });
+
+// Prevents highlighting/selecting elements on drag
+$("*").attr('unselectable', 'on')
+	.css('user-select', 'none')
+	.bind('selectstart', function () { return false; });
