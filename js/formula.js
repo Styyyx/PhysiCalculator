a840142ecdeead6520a66c7dce802eb0dabcd8b0
Styyyx <46678157+Styyyx@users.noreply.github.com>
2020-05-18 17:32:22 +0800

    Done!
    added:
    -uneven surface
    --range
    --time of flight
    --max height
    --initial height
    
    -flat surface
    --final velocity
    
    removed:
    -option to change units

--- js/formula.js
+++ js/formula.js
@@ -2,52 +2,55 @@
  * 
  * @param {number} velocity Initial velocity in m/s
  * @param {number} angle Angle of initial velocity
- * @param {boolean} radian [optional] true if given angle is in radians
  */
-function GetVelocityY(velocity, angle, radian = false) {
-  if (radian) {
-    return velocity * Math.sin(angle);
-  } else {
-    return velocity * Math.sin(ToRadian(angle));
-  }
+function GetVelocityY(velocity, angle) {
+  return velocity * Math.sin(ToRadian(angle));
 }
 
 /** Returns the initial velocity at X-Axis. Angle is converted to radians by default
  * 
  * @param {number} velocity Initial velocity in m/s
  * @param {number} angle Angle of initial velocity
- * @param {boolean} radian [optional] true if given angle is in radians
  */
-function GetVelocityX(velocity, angle, radian = false) {
-  if (radian) {
-    return velocity * Math.cos(angle);
-  } else {
-    return velocity * Math.cos(ToRadian(angle));
-  }
+function GetVelocityX(velocity, angle) {
+  return velocity * Math.cos(ToRadian(angle));
+}
+
+//#region Flat Surface
+function GetMaxHeight(velocity, angle) {
+  return ((Math.pow(GetVelocityY(velocity, angle), 2)) / (2 * 9.8));
+}
+
+function GetFlatRange(velocity, angle) {
+  return ((Math.pow(velocity, 2) * Math.sin(2 * ToRadian(angle))) / 9.8);
 }
 
 function GetFlatFlightTime(velocity, angle) {
-  return ((2 * GetVelocityY(velocity, angle)) / 9.8);
+  return ((2 * GetVelocityY(velocity, angle)) / -9.8);
 }
 
-function GetUnevenFlightTime(velocity, angle, height, radian = false) {
-  return ((GetVelocityY(velocity, angle, radian) / 9.8) + Math.sqrt(((2 * 9.8 * height) + Math.pow(GetVelocityY(velocity, angle, radian), 2)) / (Math.pow(9.8, 2))));
+function GetFlatFinalVelocity(velocity, angle, time) {
+  return Math.sqrt(Math.pow(GetVelocityX(velocity, angle), 2) + Math.pow((GetVelocityY(velocity, angle) - (9.8) * time), 2));
+}
+//#endregion
+
+//#region Uneven Surface
+function GetUnevenFlightTime(velocity, angle, height) {
+  return ((GetVelocityY(velocity, angle) / 9.8) + Math.sqrt(((2 * 9.8 * height) + Math.pow(GetVelocityY(velocity, angle), 2)) / (Math.pow(9.8, 2))));
 }
 
-function GetMaxHeight(velocity, angle, radian = false) {
-  return ((Math.pow(GetVelocityY(velocity, angle, radian), 2)) / 2 * 9.8);
+function GetUnevenRange(velocity, angle, time) {
+  return (GetVelocityX(velocity, angle) * time);
 }
 
-function GetUnevenRange(iheight, velocity, angle, radian = false) {
-  return ((Math.pow(velocity, 2) * (Math.sin(ToRadian(2 * angle))) * (1 + Math.sqrt(1 + ((2 * 9.8 * iheight) / (Math.pow(GetVelocityY(velocity, angle, radian))))))) / (2 * 9.8));
+function GetUnevenInitialHeight(velocity, angle, time) {
+  return ((((Math.pow(9.8, 2)) * (Math.pow((time - (GetVelocityY(velocity, angle) / 9.8)), 2))) - Math.pow(GetVelocityY(velocity, angle), 2)) / (2 * 9.8));
 }
+//#endregion
 
-function GetFlatRange(velocity, angle, radian = false) {
-  if (radian) {
-    return ((Math.pow(velocity, 2) * Math.sin(2 * angle)) / 9.8);
-  }
-  return ((Math.pow(velocity, 2) * Math.sin(2 * ToRadian(angle))) / 9.8);
-}
+
+
+
 
 function ToRadian(angle) {
   return (angle * Math.PI) / 180;
--- js/js-projectile.js
+++ js/js-projectile.js
@@ -8,19 +8,51 @@
 $('.panel#projectile .title select').on('change', (e) => {
   let idx = e.target.selectedIndex,
     methodSelect = $('.panel#projectile .method select');
-
-});
-
-$('.panel#projectile .form .data#angle select').on('change', (e) => {
-  $('.panel#projectile .form .data#angle div').text(($(e.target).prop('selectedIndex') == 0) ? 'Angle &theta;' : 'Radians');
+  methodSelect.empty();
+  if (idx == 0) {
+    methodSelect.append('<option>Range</option><option>Time of Flight</option><option>Max Height</option><option>Final Velocity</option>');
+  } else {
+    methodSelect.append('<option>Range</option><option>Time of Flight</option><option>Max Height</option><option>Initial Height</option>');
+  }
+  methodSelect.trigger('change');
 });
 
 $('.panel#projectile .method select').on('change', (e) => {
-  let methodIndex = $(e.target).prop('selectedIndex'),
-    resultSelect = $('.panel#projectile .result select'),
-    form = $('.panel#projectile .form');
-  if (methodIndex == 0) {
-
+  let surfaceIndex = $('.panel#projectile .title select').prop('selectedIndex'),
+    methodIndex = $(e.target).prop('selectedIndex'),
+    form = $('.panel#projectile .form'),
+    dataVi = '<div class="data" id="vi"><div>Initial Velocity</div><input><div class="unit">m/s</div></div>',
+    dataAngle = '<div class="data" id="angle"><div>Angle &theta;</div><input><div class="unit">degrees</div></div>',
+    dataIheight = '<div class="data" id="iheight"><div>Initial Height</div><input><div class="unit">meters</div></div>',
+    dataTime = '<div class="data" id="time"><div>Time</div><input><div class="unit">seconds</div></div>';
+  form.empty();
+  $('.panel#projectile .result .num').text('');
+  $('.panel#projectile .result .unit').text('');
+  if (surfaceIndex == 0) {
+    if (methodIndex == 3) {
+      form.append([dataVi, dataAngle, dataTime]);
+    } else {
+      form.append([dataVi, dataAngle]);
+    }
+  } else if (surfaceIndex == 1) {
+    switch (methodIndex) {
+      case 0: {
+        form.append([dataVi, dataAngle, dataTime]);
+        break;
+      }
+      case 1: {
+        form.append([dataVi, dataAngle, dataIheight]);
+        break;
+      }
+      case 2: {
+        form.append([dataVi, dataAngle]);
+        break;
+      }
+      case 3: {
+        form.append([dataVi, dataAngle, dataTime]);
+        break;
+      }
+    }
   }
 });
 
@@ -28,34 +60,48 @@ $('.panel#projectile #btnSolve').on('click', function () {
   if (IsFormComplete('.panel#projectile .form')) {
     let methodIndex = $('.panel#projectile .method select').prop('selectedIndex'),
       data = GetData('.panel#projectile .form'),
-      isFlat = ($('.panel#projectile .title select').prop('selectedIndex') == 0) ? true : false,
-      isRadian = ($('.panel#projectile .form .data#angle select').prop('selectedIndex') == 0) ? false : true,
-      isVInches = ($('.panel#projectile .form .data#vi select').prop('selectedIndex') == 0) ? false : true;
+      isFlat = ($('.panel#projectile .title select').prop('selectedIndex') == 0) ? true : false;
 
     console.log(data);
-    console.log(`Surface is flat? = ${isFlat}`);
-    console.log(`Angle is radian? = ${isRadian}`);
-    if (isVInches) { data.vi = data.vi * 39.37; }
     if (isFlat) {
-      if (isVInches && methodIndex != 1) {
-        $('.result select').prop('selectedIndex', 1);
-      }
       switch (methodIndex) {
         case 0: {
-          ShowResult(GetFlatRange(data.vi, data.angle, isRadian));
+          ShowResult(GetFlatRange(data.vi, data.angle), 'meters');
           break;
         }
         case 1: {
-          ShowResult(GetFlatFlightTime(data.vi, data.angle, isRadian));
+          ShowResult(GetFlatFlightTime(data.vi, data.angle), 'seconds');
           break;
         }
         case 2: {
-          ShowResult(GetMaxHeight(data.vi, data.angle, isRadian));
+          ShowResult(GetMaxHeight(data.vi, data.angle), 'meters');
+          break;
+        }
+        case 3: {
+          ShowResult(GetFlatFinalVelocity(data.vi, data.angle, data.time), 'm/s');
+          break;
+        }
+      }
+    } else {
+      switch (methodIndex) {
+        case 0: {
+          ShowResult(GetUnevenRange(data.vi, data.angle, data.time), 'meters');
+          break;
+        }
+        case 1: {
+          ShowResult(GetUnevenFlightTime(data.vi, data.angle, data.iheight), 'seconds');
+          break;
+        }
+        case 2: {
+          ShowResult(GetMaxHeight(data.vi, data.angle), 'meters');
+          break;
+        }
+        case 3: {
+          ShowResult(GetUnevenInitialHeight(data.vi, data.angle, data.time), 'meters');
         }
       }
     }
-
-    $('.panel#projectile .result div').css('user-select', 'text');
+    $('.panel#projectile .result .num').css('user-select', 'text');
   }
 });
 
@@ -74,7 +120,8 @@ function IsFormComplete(el) {
 }
 
 function ShowResult(result, unit) {
-  $('.panel#projectile .result div').text(`${Math.round((result + Number.EPSILON) * 100) / 100}`);
+  $('.panel#projectile .result .num').text(`${Math.round((result + Number.EPSILON) * 100) / 100}`);
+  $('.panel#projectile .result .unit').text(unit);
 }
 
 function GetData(el) {
